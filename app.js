124817a942a955054d73318f7d0817ccb5d93c20
Tate Redding <tateredding@gmail.com>
2023-02-21 11:42:24 -0700

    added new route to view post details

--- app.js
+++ app.js
@@ -17,23 +17,50 @@ app.get("/", (req, res) => {
         <link rel="stylesheet" href="/style.css" />
       <head>
       <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-      ${posts.map(post => `
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>
-            ${post.title}
-            <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>`).join('')}
-    </div>
-  </body>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Wizard News</header>
+          ${posts.map(post => `
+            <div class='news-item'>
+              <p>
+                <span class="news-position">${post.id}. ▲</span>
+                <a href="/posts/${post.id}">${post.title}</a>
+                <small>(by ${post.name})</small>
+              </p>
+              <small class="news-info">
+                ${post.upvotes} upvotes | ${post.date}
+              </small>
+            </div>`).join('')}
+        </div>
+      </body>
     </html>
   `;
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      <head>
+      <body>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Wizard News</header>
+          <div class="news-item">
+            <p>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <p>${post.content}</p>
+          </div>
+        </div>
+      </body>
+    </html>
+    `;
   res.send(html);
 });
 
