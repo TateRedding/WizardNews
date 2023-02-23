14710317ac7a3ea24cd281f56ab710d864e99674
Tate Redding <tateredding@gmail.com>
2023-02-23 10:23:12 -0700

    completing bonus section - organizing files, adding html-template-tag

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const postBank = require("./postBank");
+const postList = require('./views/postList');
+const html = require("html-template-tag");
+const postDetails = require('./views/postDetails');
+
 const app = express();
 
 app.use(morgan("dev"));
@@ -8,68 +12,16 @@ app.use(express.static('public'))
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-
-  const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Wizard News</title>
-        <link rel="stylesheet" href="/style.css" />
-      <head>
-      <body>
-        <div class="news-list">
-          <header><img src="/logo.png"/>Wizard News</header>
-          ${posts.map(post => `
-            <div class='news-item'>
-              <p>
-                <span class="news-position">${post.id}. ▲</span>
-                <a href="/posts/${post.id}">${post.title}</a>
-                <small>(by ${post.name})</small>
-              </p>
-              <small class="news-info">
-                ${post.upvotes} upvotes | ${post.date}
-              </small>
-            </div>`).join('')}
-        </div>
-      </body>
-    </html>
-  `;
-  res.send(html);
+  res.send(postList(posts));
 });
 
 app.get('/posts/:id', (req, res) => {
-  const id = req.params.id;
-  const post = postBank.find(id);
-
-  if (!post.id) {
-    throw new Error("Not Found");
-  }
-  const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Wizard News</title>
-        <link rel="stylesheet" href="/style.css" />
-      <head>
-      <body>
-        <div class="news-list">
-          <header><img src="/logo.png"/>Wizard News</header>
-          <div class="news-item">
-            <p>
-              ${post.title}
-              <small>(by ${post.name})</small>
-            </p>
-            <p>${post.content}</p>
-          </div>
-        </div>
-      </body>
-    </html>
-    `;
-  res.send(html);
+  const post = postBank.find(req.params.id);
+  res.send(postDetails(post));
 });
 
 app.use((err, req, res, next) => {
-  const html = `
+  const htmlString = html`
     <!DOCTYPE html>
     <html>
     <head>
@@ -85,7 +37,7 @@ app.use((err, req, res, next) => {
       </div>
     </body>
     </html>`
-  res.status(404).send(html)
+  res.status(404).send(htmlString)
 });
 
 const PORT = process.env.PORT || 3000;
